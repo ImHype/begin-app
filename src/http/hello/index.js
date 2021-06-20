99ca795499b19dab50f0ff365149eb96b25f51a7
xujunyu.joey <xujunyu.joey@bytedance.com>
2021-06-20 15:27:26 +0800

    ok

--- src/http/hello/index.js
+++ src/http/hello/index.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
 
 exports.handler = async function http(req) {
-  const name = "my-event";
-  const payload = arc.http.helpers.bodyParser(req);
   await arc.events.publish({ name, payload });
   return {
     statusCode: 200,
-    body: fs.readdirSync(__dirname),
+    body: fs.readdirSync(process.cwd()),
   };
 };
