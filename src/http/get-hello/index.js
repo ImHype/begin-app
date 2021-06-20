07e7e3971a11921f07f0cc8d4d2467efd918bfd7
xujunyu.joey <xujunyu.joey@bytedance.com>
2021-06-20 15:31:33 +0800

    ok

--- src/http/get-hello/index.js
+++ src/http/get-hello/index.js
@@ -1,8 +1,15 @@
 const fs = require("fs");
 
 exports.handler = async function http(req) {
-  return {
-    statusCode: 200,
-    body: fs.readdirSync(process.cwd()),
-  };
+  try {
+    return {
+      statusCode: 200,
+      body: fs.readdirSync(process.cwd()),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message,
+    };
+  }
 };
